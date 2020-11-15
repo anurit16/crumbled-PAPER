95c2c02259126c60ebb1be741e73d56b81abf407
anurit16 <72452994+anurit16@users.noreply.github.com>
2020-11-15 16:53:38 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -4,25 +4,27 @@ class Paper{
             isStatic:false,
             restitution:0.3,
             friction:0.5,
-            density:1.2
+            density:5.3
         }
 
         this.x=x;
         this.y=y;
         this.r=r;
         this.body = Bodies.circle(x,y,r/2,option);
+      
         World.add(myWorld,this.body);
     }
-    dispaly(){
+    display(){
         var paperpos=this.body.position;
         var angle = this.body.angle;
+        paper.scale=1;
         push();
         translate(paperpos.x,paperpos.y);
         rotate (angle);
         rectMode(CENTER);
         strokeWeight(3);
         fill(255,0,255);
-        ellipse(0,0,10,10);
+        ellipse(0,0,30,30);
         pop();
         
     }
